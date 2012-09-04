b355089a25a04525266d1db6c48112e7b2fcc224
forhappy <haipingf@gmail.com>
2012-09-04 15:11:36 +0800

    add tstring testcase.

---
+++ test/tstring-tst.c
@@ -0,0 +1,15 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  tstring-tst.c
+ *
+ *    Description:  tstring testcase.
+ *
+ *        Created:  09/04/2012 03:11:24 PM
+ *
+ *         Author:  Fu Haiping (forhappy), haipingf@gmail.com
+ *        Company:  ICT ( Institute Of Computing Technology, CAS )
+ *
+ * =============================================================================
+ */
+
