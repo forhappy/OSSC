21b631f6c90d98a9a1555927472c5d836b2f3ee7
forhappy <haipingf@gmail.com>
2012-09-05 21:31:35 +0800

    add oss_initiate_multipart_upload_request_t structure.

---
+++ include/ossc/oss_initiate_multipart_upload_request.h
@@ -0,0 +1,72 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  oss_initiate_multipart_upload_request.h
+ *
+ *    Description:  initiate multipart upload request.
+ *
+ *        Created:  09/05/2012 02:32:01 PM
+ *
+ *         Author:  Fu Haiping (forhappy), haipingf@gmail.com
+ *        Company:  ICT ( Institute Of Computing Technology, CAS )
+ *
+ * =============================================================================
+ */
+
+#ifndef _OSS_INITIATE_MULTIPART_UPLOAD_REQUEST_H
+# error Never include <ossc/oss_initiate_multipart_upload_request.h> directly
+# error use <ossc/client.h> instead.
+#endif
+
+#ifndef OSS_INITIATE_MULTIPART_UPLOAD_REQUEST_H 
+#define OSS_INITIATE_MULTIPART_UPLOAD_REQUEST_H
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <ossc/oss_object_metadata.h>
+
+typedef struct oss_initiate_multipart_upload_request_s \
+			oss_initiate_multipart_upload_request_t;
+
+struct oss_initiate_multipart_upload_request_s {
+	char *bucket_name;
+	char *key;
+	oss_object_metadata_t *object_metadata;
+
+	const char * (*get_bucket_name)(oss_initiate_multipart_upload_request_t *request);
+	const char * (*get_key)(oss_initiate_multipart_upload_request_t *request);
+	oss_object_metadata_t * (*get_object_metadata)(oss_initiate_multipart_upload_request_t *request);
+	void (*set_bucket_name)(oss_initiate_multipart_upload_request_t *request,
+			const char *bucket_name);
+	void (*set_key)(oss_initiate_multipart_upload_request_t *request,
+			const char *key);
+	void (*set_object_metadata)(oss_initiate_multipart_upload_request_t *request,
+			oss_object_metadata_t *object_metadata);
+};
+
+/* *
+ * 构造函数
+ * */
+extern oss_initiate_multipart_upload_request_t *
+initiate_multipart_upload_request_initialize(const char *bucket_name,
+		const char *key);
+
+/* *
+ * 构造函数
+ * */
+extern oss_initiate_multipart_upload_request_t *
+initiate_multipart_upload_request_initialize_with_metadata(const char *bucket_name,
+		const char *key,
+		oss_object_metadata_t *object_metadata);
+
+/* *
+ * 析构函数
+ * */
+extern void
+initiate_multipart_upload_request_finalize(
+		oss_initiate_multipart_upload_request_t *request);
+
+#endif // OSS_INITIATE_MULTIPART_UPLOAD_REQUEST_H
+
