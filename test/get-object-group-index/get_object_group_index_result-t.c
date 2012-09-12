ce0b8e77b97baf603f6fc04c91833f96e6535b30
WangWei <wangwei881116@gmail.com>
2012-09-12 14:26:46 +0800

    add multipart_object_group and get_object_group_index_result.

---
+++ test/get-object-group-index/get_object_group_index_result-t.c
@@ -0,0 +1,57 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  main testcase.
+ *
+ *        Created:  09/07/2012 04:00:23 PM
+ *
+ *         Author:  Fu Haiping (forhappy), haipingf@gmail.com
+ *        Company:  ICT ( Institute Of Computing Technology, CAS )
+ *
+ * =============================================================================
+ */
+#define _OSS_GET_OBJECT_GROUP_INDEX_RESULT_H
+#include "oss_get_object_group_index_result.h"
+#undef _OSS_GET_OBJECT_GROUP_INDEX_RESULT_H
+
+int main(int argc, const char *argv[])
+{
+	oss_get_object_group_index_result_t *result = get_object_group_index_result_initialize();
+	result->set_bucket_name(result, "bucket_name");
+	result->set_key(result, "key");
+	result->set_etag(result, "etag");
+	result->set_file_length(result, 1);
+
+	int i, part_number = 3;
+	oss_multipart_object_group_t **group = (oss_multipart_object_group_t **)malloc(sizeof(oss_multipart_object_group_t *) * part_number);
+	for (i = 0; i < part_number; i++) {
+		group[i] = multipart_object_group_initialize();
+		group[i]->set_etag(group[i], "etag");
+		group[i]->set_part_name(group[i], "part_name");
+		group[i]->set_part_number(group[i], i);
+		group[i]->set_part_size(group[i], i);
+	}
+
+	result->set_group(result, group, part_number);
+
+	group = result->get_group(result, &part_number);
+
+	printf("bucket name: %s\n", result->get_bucket_name(result));
+	printf("key: %s\n", result->get_key(result));
+	printf("etag: %s\n", result->get_etag(result));
+	printf("file_length: %u\n", result->get_file_length(result));
+
+	for(i = 0; i < part_number; i++) {
+		printf("etag = %s\npart_name = %s\npart_number = %d\npart_size = %u\n", group[i]->get_etag(group[i]), group[i]->get_part_name(group[i]), group[i]->get_part_number(group[i]), group[i]->get_part_size(group[i]));
+	}
+	
+	for(i = 0; i < part_number; i++) {
+		multipart_object_group_finalize(group[i]);
+	}
+	get_object_group_index_result_finalize(result);
+
+	return 0;
+	
+}
---
+++ test/get-object-group-index/oss_get_object_group_index_result.h
@@ -0,0 +1,64 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  oss_get_object_group_index_result.h
+ *
+ *    Description:  oss get_object_group_index_result
+ *
+ *        Created:  09/07/2012 03:22:02 PM
+ *
+ *        Company:  ICT ( Institute Of Computing Technology, CAS )
+ *
+ * =============================================================================
+ */
+
+#ifndef _OSS_GET_OBJECT_GROUP_INDEX_RESULT_H
+# error Never include <ossc/modules/oss_multipart_object_group.h> directly, use <ossc/client.h> instead.
+#endif
+
+#ifndef OSS_GET_OBJECT_GROUP_INDEX_RESULT_H
+#define OSS_GET_OBJECT_GROUP_INDEX_RESULT_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+#define _OSS_MULTIPART_OBJECT_GROUP_H
+#include "oss_multipart_object_group.h"
+#undef _OSS_MULTIPART_OBJECT_GROUP_H
+
+typedef struct oss_get_object_group_index_result_s oss_get_object_group_index_result_t;
+
+struct oss_get_object_group_index_result_s {
+	char *bucket_name;
+	char *key;
+	char *etag;
+	oss_multipart_object_group_t **group;
+	int part_number;
+	size_t file_length;
+
+	const char * (*get_bucket_name)(oss_get_object_group_index_result_t *result);
+	void (*set_bucket_name)(oss_get_object_group_index_result_t *result, const char *bucket_name);
+
+	const char * (*get_key)(oss_get_object_group_index_result_t *result);
+	void (*set_key)(oss_get_object_group_index_result_t *result, const char *key);
+
+	const char * (*get_etag)(oss_get_object_group_index_result_t *result);
+	void (*set_etag)(oss_get_object_group_index_result_t *result, const char *etag);
+
+	oss_multipart_object_group_t ** (*get_group)(oss_get_object_group_index_result_t *result, int *part_number);
+	void (*set_group)(oss_get_object_group_index_result_t *result,
+			oss_multipart_object_group_t **group, int part_number);
+	
+	size_t (*get_file_length)(oss_get_object_group_index_result_t *result);
+	void (*set_file_length)(oss_get_object_group_index_result_t *result, size_t file_length);
+
+};
+
+
+extern oss_get_object_group_index_result_t * get_object_group_index_result_initialize();
+
+extern void get_object_group_index_result_finalize(oss_get_object_group_index_result_t *result);
+
+#endif // OSS_GET_OBJECT_GROUP_INDEX_RESULT_H
+
