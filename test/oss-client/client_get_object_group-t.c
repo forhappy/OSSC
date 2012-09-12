e651f79d130564196ac1afdf60274199cf94eb1e
forhappy <haipingf@gmail.com>
2012-09-12 21:42:23 +0800

    add get_object_group.

---
+++ test/oss-client/client_get_object_group-t.c
@@ -0,0 +1,162 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  client-t.c
+ *
+ *    Description:  client testcase.
+ *
+ *        Created:  09/09/2012 02:55:40 PM
+ *
+ *         Author:  Fu Haiping (forhappy), haipingf@gmail.com
+ *        Company:  ICT ( Institute Of Computing Technology, CAS )
+ *
+ * =============================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <ossc/client.h>
+
+#define _OSS_CLIENT_H
+#include <modules/oss_client.h>
+#undef _OSS_CLIENT_H
+
+#include <curl/curl.h>
+
+/* *
+ * 初始化 oss_client_t，内部使用
+ * */
+oss_client_t *
+_client_initialize(
+		const char *access_id, size_t access_id_len,
+		const char *access_key, size_t access_key_len,
+		const char *endpoint, size_t endpoint_len)
+{
+	assert(access_id != NULL);
+	assert(access_key != NULL);
+	assert(endpoint != NULL);
+
+	size_t byte_of_char = sizeof(char);
+	oss_client_t *client = NULL;
+
+	client = (oss_client_t *)malloc(sizeof(oss_client_t));
+
+	client->access_id= (char *)malloc(byte_of_char * access_id_len+ 1);
+	client->access_key = (char *)malloc(byte_of_char * access_key_len + 1);
+	client->endpoint = (char *)malloc(byte_of_char * endpoint_len + 1);
+
+	strncpy(client->access_id, access_id, access_id_len);
+	strncpy(client->access_key, access_key, access_key_len);
+	strncpy(client->endpoint, endpoint, endpoint_len);
+
+	(client->access_id)[access_id_len] = '\0';
+	(client->access_key)[access_key_len] = '\0';
+	(client->endpoint)[endpoint_len] = '\0';
+
+	return client;
+}
+
+/* *
+ * 初始化 oss_client_t
+ * */
+oss_client_t *
+client_initialize(const char *access_id,
+		const char *access_key)
+{
+	assert(access_id != NULL);
+	assert(access_key != NULL);
+
+	size_t access_id_len = strlen(access_id);
+	size_t access_key_len = strlen(access_key);
+	size_t endpoint_len = strlen(DEFAULT_OSS_HOST);
+
+	return _client_initialize(access_id, access_id_len,
+			access_key, access_key_len,
+			DEFAULT_OSS_HOST, endpoint_len);
+}
+size_t client_get_object_callback(void *ptr, size_t size, size_t nmemb, void *stream)
+{
+	size_t r = size * nmemb;
+	fwrite(ptr, size, nmemb, stream);
+	return r;
+}
+
+/* *
+ * 获取 Object
+ * */
+oss_object_t *
+client_get_object_group(oss_client_t *client, oss_get_object_request_t *request)
+{
+
+	assert(client != NULL);
+
+	const char *bucket_name = request->get_bucket_name(request);
+	const char *key = request->get_key(request);
+	FILE *file = fopen(key, "wb");
+
+	char resource[256]     = {0};
+	//char request_line[256] = {0};
+	char url[256]          = {0};
+	char header_host[256]  = {0};
+	char header_date[128]  = {0};
+	char now[128]          = {0};
+	char header_auth[512]  = {0};
+
+	char headers[1024] = {0};
+
+	unsigned int sign_len = 0;
+
+	CURL *curl = NULL;
+	CURLcode result;
+
+
+	oss_map_t *default_headers = oss_map_new(16);
+
+	sprintf(resource, "/%s/%s", bucket_name, key);
+	sprintf(url, "%s/%s/%s", client->endpoint, bucket_name, key);
+	sprintf(header_host,"Host: %s", client->endpoint);
+	sprintf(now, "%s", oss_get_gmt_time());
+	sprintf(header_date, "Date: %s", now);
+
+	oss_map_put(default_headers, OSS_DATE, now);
+	
+	const char *sign = generate_authentication(client->access_key, OSS_HTTP_GET,
+			default_headers, NULL, resource, &sign_len);
+
+	sprintf(header_auth, "Authorization: OSS %s:%s", client->access_id, sign);
+
+	curl = curl_easy_init();
+	if (curl != NULL) {
+		struct curl_slist *http_headers = NULL;
+		curl_easy_setopt(curl, CURLOPT_URL, url);
+		curl_easy_setopt(curl, CURL_HTTP_VERSION_1_1, 1L);
+		//curl_easy_setopt(curl, CURLOPT_HEADER, 1L);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, client_get_object_callback);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
+
+
+		http_headers = curl_slist_append(http_headers, header_host);
+		http_headers = curl_slist_append(http_headers, header_date);
+		http_headers = curl_slist_append(http_headers, header_auth);
+
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, http_headers);
+		curl_easy_perform(curl);
+
+		curl_slist_free_all(http_headers);
+		curl_easy_cleanup(curl);
+	}
+
+	fclose(file);
+	return NULL;
+}
+
+int main()
+{
+
+	const char *access_id = "ACSGmv8fkV1TDO9L";
+	const char *access_key = "BedoWbsJe2";
+	const char *bucket_name = "bucketname001";
+	const char *key = "a_group_file.dat";
+		oss_client_t *client = client_initialize(access_id, access_key);
+	oss_get_object_request_t *request = get_object_request_initialize(bucket_name, key);
+	client_get_object_group(client, request);
+}
